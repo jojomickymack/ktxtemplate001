4207133b9880838d368ca11e37a4f2525ef9f081
jojo <jojomickymack@gmail.com>
2019-02-09 08:01:59 -0700

    removing import to kotlin coroutines since it's enabled by default since kotlin 1.3

--- core/src/com/central/Application.kt
+++ core/src/com/central/Application.kt
@@ -3,13 +3,10 @@ package com.central
 import com.badlogic.gdx.Screen
 import com.central.screens.Game
 import ktx.app.KtxGame
-import ktx.async.enableKtxCoroutines
 
 class Application : KtxGame<Screen>() {
 
     override fun create() {
-        enableKtxCoroutines(asynchronousExecutorConcurrencyLevel = 1)
-
         val game = Game(this)
 
         addScreen(game)

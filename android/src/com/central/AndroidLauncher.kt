1a63353b4f3f6441c2a4943e097ccad81f3dd31e
jojo <jojomickymack@gmail.com>
2019-03-17 20:43:19 -0600

    playing with the android 'run' task made me realize the appId and activity name in the AndroidManifest.xml were wrong

--- android/src/com/central/AndroidLauncher.kt
+++ android/src/com/central/AndroidLauncher.kt
@@ -1,7 +1,6 @@
 package com.central
 
 import android.os.Bundle
-
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
